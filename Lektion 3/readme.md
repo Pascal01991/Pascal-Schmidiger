ad4f1f1c96e4b38d13cd3e396ae8de07763490fb
Pascal Schmidiger <pascalino.schmidiger@gmail.com>
2026-03-05 23:00:30 +0100

    Struktur aufgeräumt

---
+++ Lektion 3/readme.md
@@ -0,0 +1,3 @@
+## ES6 Umbau
+
+Files von Lektion 3 kipiert und auf ES6 umgebaut
---
+++ Lektion 3/sum.test.js
@@ -0,0 +1,19 @@
+const sum = require("./sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("array filter", () => {
+  const items = [10, 20, 30, 40, 50];
+  const restult = items.filter((i) => i > 30);
+  expect(restult).toStrictEqual([40, 50]);
+});
+
+test("array pop", () => {
+  const items = [10, 20, 30, 40, 50];
+  items.pop = 55;
+  items.push = 66;
+  const restult = items[4];
+  expect(restult).toEqual(50);
+});
