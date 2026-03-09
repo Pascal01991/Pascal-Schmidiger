54717ad5882f5d2f90640a76015391b19ae5bc6f
Pascal Schmidiger <pascalino.schmidiger@gmail.com>
2026-03-09 21:57:30 +0100

    .eslintrc.json gemäss Unterlagen

--- Lektion 4/car.js
+++ Lektion 4/car.js
@@ -1,4 +1,9 @@
 export class Car {
-  Make = "Volkswagen";
+  Marke = "Volkswagen";
   Model = "Jetta";
+
+  toJSON() {
+    const { Marke, ...rest } = this;
+    return rest; // without "Marke"
+  }
 }
